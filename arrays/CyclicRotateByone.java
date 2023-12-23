ace883e4c3394ad26dbd7e2d22d475b6afb6387e
sanjeev <63443262+Sanjeev75079982@users.noreply.github.com>
2023-12-23 14:08:11 +0530

    keep last ele in temp and keep shifting from right to left Utils

---
+++ arrays/CyclicRotateByone.java
@@ -0,0 +1,32 @@
+/*Given an array, rotate the array by one position in clock-wise direction.
+
+    Example 1:
+        Input:
+        N = 5
+        A[] = {1, 2, 3, 4, 5}
+        Output:
+        5 1 2 3 4
+*/
+
+package arrays;
+
+public class CyclicRotateByone {
+    
+    public static void rotate(int arr[], int n)
+    {
+        int temp = arr[n - 1];
+        
+        //shifting the element by one place from left to right
+        for(int i = n - 1; i > 0; i--){
+            arr[i] = arr[i-1]; 
+        }
+        arr[0] = temp;
+    }
+
+    public static void main(String[] args) {
+        int arr1[] = {3,4,5,6,7,8,9};
+        Utils.print(arr1, "Input Array");
+        rotate(arr1, arr1.length);
+        Utils.print(arr1, "Rotated Array");
+    }
+}
